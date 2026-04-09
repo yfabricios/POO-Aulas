c56dd971cad99e88aa101412e5e630746eb5d398
Fabricios <fabriciooaraujo8@gmail.com>
2026-04-09 14:21:03 -0300

    cod 8

---
+++ Aula-05/q1.py
@@ -0,0 +1,34 @@
+class Retangulo:
+    def __init__(self):
+        self.__base = 0        # atributos escondidos / atributos encapsulados
+        self.__altura = 0
+    
+    def set_base(self, valor):
+        if valor < 0: 
+            raise ValueError('Valor deve ser positivo')
+        self.__base = valor
+
+    def get_base(self):
+        return self.__base
+
+    def set_altura(self, valor):
+        if valor < 0:
+             raise ValueError('Valor deve ser positivo')
+        self.__altura = valor
+
+    def get_altura(self):
+        return self.__altura
+
+    def diagonal(self):
+        return (self.__base ** 2 + self.__altura ** 2) ** (1/2)
+
+class UI:
+    def main():
+        r = Retangulo()
+        r.set_base(float(input('Base: ')))
+        r.set_altura(float(input('altura: ')))
+        print(f'O retângulo de base = {r.get_base()}, e altura = {r.get_altura()} ')
+        diagonal = r.diagonal()
+        print(f'tem diagonal = {diagonal}')
+
+UI.main()
